7462448005548551f0eff06e95f6f3dfeb6faa11
Ignust <andrey.las90@gmail.com>
2019-06-10 19:43:17 +0300

    [odessa] fix checkButtons

--- Tetris/tetrisElements/Keyboard.cpp
+++ Tetris/tetrisElements/Keyboard.cpp
@@ -1,4 +1,7 @@
 #include <stdio.h>
+#include <termios.h>    //struct termios
+#include <unistd.h>     //STDIN_FILENO
+#include<cstdint>       //uint8_t
 
 
 #include "Keyboard.hpp"
@@ -23,7 +26,16 @@ Keyboard::~Keyboard()
 bool Keyboard::checkButtons()
 //-----------------------------------------------------------------------------
 {
-    const int button = getchar();
+    struct termios oldt;
+    struct termios newt;
+    uint8_t button;
+    tcgetattr( STDIN_FILENO, &oldt );
+    newt = oldt;
+    newt.c_lflag &= ~( ICANON | ECHO );
+    tcsetattr( STDIN_FILENO, TCSANOW, &newt );
+    button = static_cast<uint8_t>(getchar());
+    tcsetattr( STDIN_FILENO, TCSANOW, &oldt );
     printf("Button %d = %c was pressed\n", button, static_cast<char>(button));
+
     return false;
 }
